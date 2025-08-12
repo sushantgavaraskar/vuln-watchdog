a778e0a3b28a58b386351b0da797f1fe04729ac8
Sushant Gavaraskar <sushantgavaraskar18@gmail.com>
2025-08-12 18:09:30 +0530

    fix tailwind issue

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-900`}>
+      <body className={inter.className}>
         <AuthProvider>
           {children}
           <Toast />
